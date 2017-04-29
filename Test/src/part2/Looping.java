7fa6402b933a7ae215e28826f87cacf789ae78ee
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-04-29 11:41:55 +0800

    Test commit from Dania

--- Test/src/part2/Looping.java
+++ Test/src/part2/Looping.java
@@ -9,52 +9,6 @@ package part2;
  */
 public class Looping {
 	 public static void main (String[] args) {
-	      int sum = 0;          // Store the accumulated sum, init to 0
-	      double average = 0;       // average in double
-	      int lowerbound = 1;   // The lowerbound to sum
-	      int upperbound = 100; // The upperbound to sum
-	      
-	      // Use a for-loop to sum from lowerbound to upperbound
-	      for (int number = lowerbound; number <= upperbound; ++number) {
-	         sum += number; // same as "sum = sum + number"
-		     average =  (double) sum/ number;
-	       }  	     
-	   		System.out.println(average);
-		 	System.out.println(sum);
-		 	
-		 	main1();
-		 	main2();
 	 	}
 	 
-	 public static void main1 () {
-	      int sum = 0;         
-	      double average = 0;    
-	      int lowerbound = 1;  
-	      int upperbound = 100; 
-	      
-	      int number = lowerbound;
-
-	      while (number <= upperbound) {
-	    	   sum += number;
-	    	   ++number;
-	    	}	     
-	   		System.out.println("here" + average);
-		 	System.out.println(sum);
-	 	}
-	 
-	 public static void main2 () {
-	      int sum = 0;         
-	      double average = 0;      
-	      int lowerbound = 1; 
-	      int upperbound = 100; 
-	      
-	      int number = lowerbound;
-
-	      while (number <= upperbound) {
-	    	   sum += number;
-	    	   ++number;
-	    	}	
-	   		System.out.println("here" + average);
-		 	System.out.println(sum);
-	 	}
-}
+	 }
