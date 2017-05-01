6c469fd72341315779b7573f2c455a0dbac3ce0a
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-01 23:54:31 +0800

    commit

--- Test/src/part1/CheckOddEven.java
+++ Test/src/part1/CheckOddEven.java
@@ -1,17 +1,29 @@
 package part1;
 
+import java.util.Scanner;
+
 public class CheckOddEven {
 
 	// update the program to let the user insert the number, then check if 0 or
 	// odd or even
+
+	static Scanner nextInput = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		int a = 9;
+		int x;
+		x = nextInput.nextInt();
 
-		if (a == 0) {
-			System.out.println("Even Number");
-		} else {
-			System.out.println("Odd Number");
+		if (x == 0) 
+		{
+			System.out.println("Number is Zero : " + x);
 		}
-
+		else if (x%2 != 0)
+		{
+			System.out.println("Number is Odd : " + x);
+		}
+		else if (x%2 == 0)
+		{
+			System.out.println("Number is Even : " + x);
+		}
 	}
 }
