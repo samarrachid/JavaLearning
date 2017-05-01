880cd4b45d9d27723eb6837e3980cf96ae2534c8
srachid <srachid@menard-inc.com>
2017-05-01 11:06:57 -0500

    test

--- Test/src/part3/Functions.java
+++ Test/src/part3/Functions.java
@@ -1,7 +1,27 @@
 package part3;
 
 public class Functions {
-	 public static void main (String[] args) {
-		   
-	   }
+	public static void main(String[] args) {
+		Student[] students = new Student[] { 
+				new Student("Morgan", "Freeman"), 
+				new Student("Brad", "Pitt"),
+				new Student("Kevin", "Spacey"), };
+		for (Student s : students) {
+			s.printFullName();
+		}
+	}
+}
+
+class Student {
+	private String firstName;
+	private String lastName;
+
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void printFullName() {
+		System.out.println(this.firstName + " " + this.lastName);
+	}
 }
