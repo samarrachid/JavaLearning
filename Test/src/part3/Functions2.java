e4525358c4dc4610e44e6b15cc5680a065c3766a
Samar Rachid <samar_svu@hotmail.com>
2017-05-01 20:09:39 -0500

    Function method

--- Test/src/part3/Functions2.java
+++ Test/src/part3/Functions2.java
@@ -2,32 +2,32 @@ package part3;
 
 public class Functions2 {
 	 public static void main (String[] args) {
-		   int i = 9;
-		   int y = 78;
-		   
-		   test(i, y);
-		   test1(i, y);
-		   test2(i, y);
-		   test3(i, y);
-	   }
-	 
-	 public static void test(int i, int gf) {
-		 int sum = i + gf;
+		 test(4, 6);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test(6,9);
+		 test1(6,9);
+		 
+		 int d = 5;
+		 System.out.println("d in main" + d);
+	 }
+
+	 public static void test(int d, int t) {
+		 int sum = d + t;
 		 System.out.println("Test sum" + sum);
 	 }
 	 
-	 public static void test1(int i, int gf) {
-		 int result = i - gf;
-		 System.out.println("Test 1" + result);
+	 public static void test1(int d, int t) {
+		 int sum = d - t;
+		 System.out.println("Test " + sum);
 	 }
 	 
-	 public static void test2(int i, int gf) {
-		 int result = i / gf;
-		 System.out.println("Test 2 " + result);
-	 }
 	 
-	 public static void test3(int i, int gf) {
-		 int result = i * gf;
-		 System.out.println("Test 3" + result);
-	 }
+	 
 }
