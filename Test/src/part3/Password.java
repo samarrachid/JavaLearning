f9f8f22cd3868063d33289369327c4763ee5a8d4
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-03 20:59:50 +0800

    commit

---
+++ Test/src/part3/Password.java
@@ -0,0 +1,39 @@
+package part3;
+
+import java.util.Scanner;
+
+public class Password {
+	/**
+	 * rite a Java method to check whether a string 
+	 * is a valid password.  
+Password rules:
+A password must have at least ten characters.
+A password consists of only letters and digits.
+A password must contain at least two digits.
+	 */
+
+	static Scanner input = new Scanner(System.in);
+    public static void main(String args[]) {
+
+    		System.out.println("Enter password:");
+    		String password = input.nextLine();
+    		if (password.length() < 10) {
+    			System.out.println("password must have 10 chars at least");
+    		//	if (Character.LetterOrDigit(password.charAt)) {
+    				
+    		int count = 0;
+    		
+    		
+    		
+    		}
+    		
+    	
+    	
+
+       
+
+   }
+
+   }
+
+
