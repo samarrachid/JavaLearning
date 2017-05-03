6ef50cca1b9cd49ead9a32ca8052f5c81872dc10
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-03 21:29:08 +0800

    hj

---
+++ Test/src/part4/PracticeTwo.java
@@ -0,0 +1,27 @@
+package part4;
+
+import java.util.Scanner;
+
+public class PracticeTwo {
+	public static void main(String[] args){
+		int number;
+		do {
+		 Scanner input = new Scanner(System.in);
+		 String user = input.nextLine();
+		 while (!input.hasNextInt()) {
+			 System.out.println("this is not");
+			 input.next();
+		 }
+		 number =input.nextInt();
+		} while (number <= 0);
+		System.out.println("tnq" + number);
+			 
+		 
+		 
+		
+		
+	}
+	
+			            }
+
+
