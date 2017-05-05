ca993982acf3fc6b42578e033e4812e4ae1fd7cb
srachid <srachid@menard-inc.com>
2017-05-05 16:29:24 -0500

    Added date class

---
+++ Test/src/part4/Date.java
@@ -0,0 +1,22 @@
+package part4;
+
+public class Date {
+
+	//get the last day of the current month
+	
+	// get current full date and time
+	
+	// get current full date
+	
+	// get  12 december 2014
+	
+	//get current year
+	
+	//
+	
+	//
+	
+	//
+	
+	
+}
