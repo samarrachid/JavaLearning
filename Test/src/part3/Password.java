e837b819611155f160360443e39cbbf1bc16e69d
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-09 22:50:09 +0800

    commit

--- Test/src/part3/Password.java
+++ Test/src/part3/Password.java
@@ -3,37 +3,53 @@ package part3;
 import java.util.Scanner;
 
 public class Password {
+
 	/**
-	 * rite a Java method to check whether a string 
-	 * is a valid password.  
-Password rules:
-A password must have at least ten characters.
-A password consists of only letters and digits.
-A password must contain at least two digits.
+	 * rite a Java method to check whether a string is a valid password.
+	 * Password rules: A password must have at least ten characters. A password
+	 * consists of only letters and digits. A password must contain at least two
+	 * digits.
 	 */
 
 	static Scanner input = new Scanner(System.in);
-    public static void main(String args[]) {
 
-    		System.out.println("Enter password:");
-    		String password = input.nextLine();
-    		if (password.length() < 10) {
-    			System.out.println("password must have 10 chars at least");
-    		//	if (Character.LetterOrDigit(password.charAt)) {
-    				
-    		int count = 0;
-    		
-    		
-    		
-    		}
-    		
-    	
-    	
+	public static void main(String args[]) {
+		String password = "0";
+		length10(input, password);
+		digitOrLetter(input, password);
 
-       
+	}
 
-   }
+	public static void length10(Scanner input, String password) {
+		System.out.println("Enter password:");
+		password = input.nextLine();
+		while (password.length() < 10) {
 
-   }
+			System.out.println("password must have 10 chars at least");
+			// if (Character.LetterOrDigit(password.charAt)) {
+			password = input.nextLine();
 
+		}
 
+	}
+
+	public static void digitOrLetter(Scanner input, String password) {
+		int digit = 0;
+		if (!Character.isLetterOrDigit(password.charAt(0))) {
+			System.out.println("Enter digit or letter only!!");
+			password = input.nextLine(); }
+			while (Character.isDigit(password.charAt(0))) {
+				digit++; 
+				if (digit < 2) {
+					System.out.println("your password must contain at least 2 digits!");
+					password = input.nextLine();
+				}
+
+				}
+				
+			
+		}
+
+	
+
+}
