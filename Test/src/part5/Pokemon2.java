02e1c0464db5904c820d555476bcd85bea0a2194
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-11 21:18:29 +0800

    commit

---
+++ Test/src/part5/Pokemon2.java
@@ -0,0 +1,12 @@
+package part5;
+
+public class Pokemon2 {
+	public static void main(String[] args) {
+		// i still don't get the main point of this but i guess it's something big
+		
+		Pokemon Pokemon = new Pokemon() ;
+		System.out.println("My name is: " + Pokemon.name + " and my health is: " + Pokemon.getHealth());
+		
+	}
+
+}
