35071308f64aee35ec47b448b36f2da31e4f0fad
Dania Rachid <Dania Rachid@DESKTOP-6H4O8B1>
2017-05-15 16:38:14 +0800

    commit

---
+++ Test/src/part5/Bicycle.java
@@ -0,0 +1,34 @@
+package part5;
+
+public class Bicycle {
+	
+	int speed;
+	int brake;
+	int gear;
+	int cadence;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int speedUp(int speed) {
+		return speed;
+	}
+	
+	public static int speedDown(int speed) {
+		return speed;
+	}
+	
+	public static int moveForward (int cadence) {
+		return cadence;
+	}
+	
+	public static int moveBack (int cadence) {
+		return cadence;
+	}
+	
+	public static int setBrake(int brake) {
+		return brake;
+	}
+}
+
